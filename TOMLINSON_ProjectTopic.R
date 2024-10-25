051fe3120f1653b9ebcd69b42a46fb2d3df7482e
zoebtomlinson1@gmail.com <itszoetom>
2024-10-25 15:47:25 -0700

    initial topic exploration assignment

---
+++ TOMLINSON_ProjectTopic.R
@@ -0,0 +1,35 @@
+'''
+General Topic of Interest Related to the Oregon Bee Atlas Data:
+
+Exploring the impact of forest fires on bee populations in Oregon, with a focus 
+on potential correlations between fire incidents and bee population dynamics, 
+such as diversity patterns, relocation behaviors, and potential sampling biases.
+
+Research Questions
+
+How do forest fires influence the geographic relocation of bee populations?
+
+What impact do forest fires have on the diversity of bee species in affected 
+regions?
+
+Are there shifts in bee diet patterns following forest fires due to changes in 
+flora?
+
+Additional Relevant Datasets
+Oregon Department of Forestry Fire Occurrence Data: This dataset, spanning from 
+2000-2022, offers detailed information on wildfire incidents across Oregon, 
+including location, date, and fire causes. This data is accessible at: ODF Fire 
+Occurrence Data.
+
+Additional Potential Sources: 
+Federal or academic repositories like the National Interagency Fire Center 
+(NIFC) for multi-state fire data, and environmental data resources such as 
+EPA Air Quality data, which could be correlated to bee relocation and health 
+post-fire.
+
+Partner Name: Madi Kaloberdanz
+Contribution: Madi contributed to brainstorming research questions and 
+identifying relevant datasets for our analysis.
+
+'''
+
